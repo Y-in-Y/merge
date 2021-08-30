803a581f147fd252c7f6f0d6e0757bcc3aa92bac
3Cat-jaguar <ryusa13@gmail.com>
2021-08-30 23:06:22 +0900

    norm check cd

--- run/blt/builtin_cd.c
+++ run/blt/builtin_cd.c
@@ -2,6 +2,34 @@
 
 extern t_env	*g_env_list;
 
+void	go_to_home(void)
+{
+	char	*pwd;
+	t_env	*tmp;
+
+	tmp = g_env_list;
+	pwd = NULL;
+	while (tmp)
+	{
+		if (ft_strncmp(tmp->name, "HOME", 5) == 1)
+		{
+			pwd = tmp->value;
+			break ;
+		}
+		tmp = tmp->next;
+	}
+	if (!pwd)
+	{
+		printf("cd: HOME not set\n");
+		g_env_list->exit_code = 1;
+		return ;
+	}
+	chdir(pwd);
+	pwd = getcwd(NULL, 0);
+	run_export("PWD", pwd);
+	g_env_list->exit_code = 0;
+}
+
 void	builtin_cd(t_all *a)
 {
 	char	*pwd;
@@ -12,30 +40,17 @@ void	builtin_cd(t_all *a)
 	pwd = getcwd(NULL, 0);
 	run_export("OLDPWD", pwd);
 	if (!a->arg[1])
-	{
-		tmp = g_env_list;
-		while (tmp)
-		{
-			if (ft_strncmp(tmp->name, "HOME", 5) == 1)
-			{
-				pwd = tmp->value;
-				break ;
-			}
-			tmp = tmp->next;
-		}
-		chdir(pwd);
-		pwd = getcwd(NULL, 0);
-		run_export("PWD", pwd);
-	}
+		go_to_home();
 	else
 	{
 		if (chdir(a->arg[1]) == -1)
 		{
 			printf("%s is not a directory\n", a->arg[1]);
+			g_env_list->exit_code = 1;
 			return ;
 		}
 		pwd = getcwd(NULL, 0);
 		run_export("PWD", pwd);
+		g_env_list->exit_code = 0;
 	}
-	g_env_list->exit_code = 0;
 }
