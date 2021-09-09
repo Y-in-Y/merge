d0d9673d9e3ca9f2e82c21bf2b185ab7d8b4a577
Yuri Lee <ylee@c7r1s1.42seoul.kr>
2021-09-09 11:53:57 +0900

    complete norm check

--- run/run_shell/multipipe.c
+++ run/run_shell/multipipe.c
@@ -14,70 +14,42 @@
 
 extern t_env	*g_env_list;
 
-void	multipipe(t_all *a)
+void	make_process(t_all *a, int cmd_cnt, int **fd, pid_t *pid)
 {
-	int		cmd_cnt;
-	pid_t	*pid;
-	int		**fd;
-	int		state;
-	int		i;
+	int	i;
 
-	cmd_cnt = a->pipe_cnt + 1;
-	pid = (pid_t *)malloc(sizeof(pid_t) * cmd_cnt);
-	fd = (int **)malloc(sizeof(int *) * cmd_cnt);
 	i = 0;
 	while (i < cmd_cnt)
 	{
-		fd[i] = (int *)malloc(sizeof(int) * 2);
-		i++;
-	}
-	i = 0;
-	if (pipe(fd[i]) < 0)
-		error_msg("pipe error");
-	pid[i] = fork();
-	if (pid[i] < 0)
-		error_msg("fork error");
-	if (i == 0 && pid[i] == 0)
-		child_process(a, i, fd);
-	else
-	{
-		i = 1;
-		while (i < cmd_cnt - 1)
+		if (i < cmd_cnt - 1 && pipe(fd[i]) < 0)
+			error_msg("pipe error");
+		pid[i] = fork();
+		if (pid[i] < 0)
+			error_msg("fork error");
+		else if (pid[i] == 0)
 		{
-			if (pipe(fd[i]) < 0)
-				error_msg("pipe error");
-			pid[i] = fork();
-			if (pid[i] < 0)
-				error_msg("fork error");
-			if (pid[i] == 0)
-				break ;
+			child_process(a, i, fd);
+			exit(0);
+		}
+		else if (i > 0)
+		{
 			close(fd[i - 1][0]);
 			close(fd[i - 1][1]);
-			i++;
 		}
-		if (i < cmd_cnt - 1 && pid[i] == 0)
-			child_process(a, i, fd);
-		else
-		{
-			if (i != cmd_cnt - 1)
-				error_msg("unknown error check multipipe");
-			pid[i] = fork();
-			if (pid[i] < 0)
-				error_msg("fork error");
-			if (pid[i] == 0)
-				child_process(a, i, fd);
-			else
-			{
-				close(fd[i - 1][0]);
-				close(fd[i - 1][1]);
-				i = 0;
-				while (i < cmd_cnt)
-				{
-					waitpid(pid[i], &state, 0);
-					i++;
-				}
-			}
-		}
+		i++;
+	}
+}
+
+void	after_fork(int cmd_cnt, pid_t *pid, int **fd)
+{
+	int	i;
+	int	state;
+
+	i = 0;
+	while (i < cmd_cnt)
+	{
+		waitpid(pid[i], &state, 0);
+		i++;
 	}
 	i = 0;
 	while (i < cmd_cnt)
@@ -94,4 +66,24 @@ void	multipipe(t_all *a)
 	else
 		g_env_list->exit_code = 0;
 	exit(g_env_list->exit_code);
+}
+
+void	multipipe(t_all *a)
+{
+	int		cmd_cnt;
+	pid_t	*pid;
+	int		**fd;
+	int		i;
+
+	cmd_cnt = a->pipe_cnt + 1;
+	pid = (pid_t *)malloc(sizeof(pid_t) * cmd_cnt);
+	fd = (int **)malloc(sizeof(int *) * cmd_cnt);
+	i = 0;
+	while (i < cmd_cnt)
+	{
+		fd[i] = (int *)malloc(sizeof(int) * 2);
+		i++;
+	}
+	make_process(a, cmd_cnt, fd, pid);
+	after_fork(cmd_cnt, pid, fd);
 }
